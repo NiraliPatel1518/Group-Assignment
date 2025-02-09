7623e90a17ef55251c7f66029f7bcf8ac936a658
Archi Ramoiya <ramoliar@sheridancollege.ca>
2025-02-09 14:03:12 -0500

    Changed player.java

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -5,45 +5,41 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
  *
  * @author dancye
  * @author Paul Bonenfant Jan 2020
  */
-public abstract class Player {
+public class Player {
+    private List<Card> hand;
 
-    private String name; //the unique name for this player
-
-    /**
-     * A constructor that allows you to set the player's unique ID
-     *
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name) {
-        this.name = name;
+    public Player() {
+        hand = new ArrayList<>();
     }
 
-    /**
-     * @return the player name
-     */
-    public String getName() {
-        return name;
+    public void addCard(Card card) {
+        hand.add(card);
     }
 
-    /**
-     * Ensure that the playerID is unique
-     *
-     * @param name the player name to set
-     */
-    public void setName(String name) {
-        this.name = name;
+    public Card playCard(String cardToPlay) {
+        for (Card card : hand) {
+            if (card.toString().equals(cardToPlay)) {
+                hand.remove(card);
+                return card;
+            }
+        }
+        return null;
     }
 
-    /**
-     * The method to be overridden when you subclass the Player class with your specific type of Player and filled in
-     * with logic to play your game.
-     */
-    public abstract void play();
+    public List<Card> getHand() {
+        return hand;
+    }
 
+    public void clearHand() {
+        hand.clear();
+}
 }
