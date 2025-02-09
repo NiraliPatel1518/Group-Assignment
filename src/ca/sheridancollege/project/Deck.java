a512fa9254a02785e68f8aba24bf05544ef7c7a9
Archi Ramoiya <ramoliar@sheridancollege.ca>
2025-02-09 14:14:17 -0500

    Changed Deck.java

--- src/ca/sheridancollege/project/Deck.java
+++ src/ca/sheridancollege/project/Deck.java
@@ -5,6 +5,10 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 
 /**
  * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
@@ -14,6 +18,30 @@ package ca.sheridancollege.project;
  * @author Paul Bonenfant Jan 2020
  */
 public class Deck {
+private List<Card> cards;
 
-    //The group of cards, stored in an ArrayList
-}//end class
+    public Deck() {
+        cards = new ArrayList<>();
+        String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"};
+
+        for (String suit : suits) {
+            for (String rank : ranks) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+    }
+
+    public void shuffle() {
+        Collections.shuffle(cards);
+    }
+
+    public Card dealCard() {
+        return cards.remove(cards.size() - 1);
+    }
+
+    public int size() {
+        return cards.size();
+}
+}
+   //end class
